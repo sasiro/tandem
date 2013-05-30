bdd7f246f629a880bec52220cbd13db2f22e2d39
Victor Donet <victordp86@gmail.com>
2013-05-30 21:08:44 +0200

    Just started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tandem::Application.config.secret_token = '67010071ef72a07eff5b453eb6e6f6dc9b88a539a500dc26641836adcc519f9c10b1138f09eaa70d320ab6866645147b575a76e9a2e10027346a41e460c180f7'
