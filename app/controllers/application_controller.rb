0fc8ab3ac8cfc876836eccbfcee854a3bf3e90e8
Victor Donet <victordp86@gmail.com>
2013-06-20 06:45:42 +0200

    Calendar deleted from sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  after_filter :store_location
+
+def store_location
+  # store last url as long as it isn't a /users path
+  session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+end
+
+def after_sign_in_path_for(resource)
+   users_path
+end
 end
