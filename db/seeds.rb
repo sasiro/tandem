7e33169691cd677ca408a1546122b79c857986fc
Victor Donet <victordp86@gmail.com>
2013-10-14 14:59:54 +0200

    Seed file filled with Interest and Languages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Interest.create(name:'Sports') unless Interest.find_by_name('Sports')
+Interest.create(name:'Techlology') unless Interest.find_by_name('Techlology')
+Interest.create(name:'Arts') unless Interest.find_by_name('Arts')
+Interest.create(name:'Health') unless Interest.find_by_name('Health')
+Interest.create(name:'Music') unless Interest.find_by_name('Music')
+Interest.create(name:'History') unless Interest.find_by_name('History')
+
+Language.create(name:'Spanish') unless Language.find_by_name('Spanish')
+Language.create(name:'Portuguese') unless Language.find_by_name('Portuguese')
+Language.create(name:'English') unless Language.find_by_name('English')
+Language.create(name:'Italian') unless Language.find_by_name('Italian')
+Language.create(name:'French') unless Language.find_by_name('French')
+Language.create(name:'German') unless Language.find_by_name('German')
+
