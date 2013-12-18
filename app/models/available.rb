d8e187224ba0ab1d55590acf985a11aa74bdcc03
Victor Donet <victordp86@gmail.com>
2013-12-18 18:07:53 +0100

    When availables destroyed appointment too

--- app/models/available.rb
+++ app/models/available.rb
@@ -1,6 +1,6 @@
 class Available < ActiveRecord::Base
   attr_accessible :day, :duration, :starts
-  has_many :appointments
+  has_many :appointments, dependent: :destroy
   has_many :users, :through => :appointments
   DAYS = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday]
   def start_end_time
