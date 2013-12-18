aa86f2e5d05460789812f807bb9fbd2dfc5384c0
Victor Donet <victordp86@gmail.com>
2013-12-18 16:09:24 +0100

    Availables index view updated

--- app/models/available.rb
+++ app/models/available.rb
@@ -2,7 +2,7 @@ class Available < ActiveRecord::Base
   attr_accessible :day, :duration, :starts
   has_many :appointments
   has_many :users, :through => :appointments
-  DAYS = %w[Monday Tuesday Wednesday Thursday Friday Saturnday Sunday]
+  DAYS = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday]
   def start_end_time
     '' +self.starts.strftime("%I:%M%p") + '-' + ((self.starts + self.duration)).strftime("%I:%M%p")+''
 
