2ded1fc2fc4a2b14283ce880b78fe10490eb834f
Victor Donet <victordp86@gmail.com>
2014-02-20 12:48:24 +0100

    modified button's size

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -32,7 +32,7 @@ class AppointmentMailer < ActionMailer::Base
       @text1 = "Hello #{@user.name},<br /> <br /> Your tandem  has been canceled!"  
       @text2 = "Canceled"
       @text3 = "No worries there are a looot of users looking for more language exchanges in Tandem Spot"
-      @img1 = "bt.png"
+      @img1 = "bt3.png"
 
       msj = @appointment.status 
       mail to: "#{user.name} <#{user.email}>", subject:"#{owner.name} has #{msj} a tandem with you"
@@ -49,7 +49,7 @@ class AppointmentMailer < ActionMailer::Base
       @text1 = "Hello #{@owner.name},<br /> <br /> Congratulations you have a tandem request! :) \n #{@user.name} wants to speak with you."  
       @text2 = "#{@day} <br />#{@time}"
       @text3 = "Remember to login to Tandem Spot 10 minutes before your appointment and go to Speak Now"
-      @img1 = "bt3.png"
+      @img1 = "bt2.png"
       msj = "requested"  
       mail to: "#{owner.name} <#{owner.email}>", subject:"#{user.name} has #{msj} a tandem with you"
     elsif @appointment.status =="accepted"
@@ -63,7 +63,7 @@ class AppointmentMailer < ActionMailer::Base
       @text1 = "Hello #{@owner.name},<br /> <br /> You have refused a tandem. We hope that you are busy doing funny things and you are not delaying your language learning forever... "  
       @text2 = "Canceled!"
       @text3 = "We are sure that you can find a great language partner that fits you."
-      @img1 = "bt2.png"
+      @img1 = "bt3.png"
       msj = @appointment.status 
       mail to: "#{owner.name} <#{owner.email}>", subject:"You have #{msj} a tandem with #{user.name}"
     end
