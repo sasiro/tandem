50597b98d42ed4266be27a792059efeb5089e9e3
Victor Donet <victordp86@gmail.com>
2014-02-20 11:06:27 +0100

    button changing depending on mail

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -16,6 +16,7 @@ class AppointmentMailer < ActionMailer::Base
       @text1 = "Hello #{@user.name},<br /> <br /> Your tandem request have been sent to #{@owner.name} :)"  
       @text2 = "#{@day} <br />#{@time}"
       @text3 = "While you wait for #{@owner.name}'s confirmation you can meet some other interesting people"
+      @img1 = "bt.png"
      
       msj = "requested"  
       mail to: "#{user.name} <#{user.email}>", subject:"You have #{msj} a tandem with #{owner.name}" 
@@ -23,6 +24,7 @@ class AppointmentMailer < ActionMailer::Base
       @text1 = "Hello #{@user.name},<br /> <br /> Your tandem with #{@user.name} have been accepted! :) "  
       @text2 = "#{@day} <br />#{@time}"
       @text3 = "Remember to login to Tandem Spot 10 minutes before your appointment and go to Speak Now"
+      @img1 = "bt.png"
       msj = @appointment.status 
       
       mail to: "#{user.name} <#{user.email}>", subject:"#{owner.name} has #{msj} a tandem with you"
@@ -30,7 +32,8 @@ class AppointmentMailer < ActionMailer::Base
       @text1 = "Hello #{@user.name},<br /> <br /> Your tandem  has been canceled!"  
       @text2 = "Canceled"
       @text3 = "No worries there are a looot of users looking for more language exchanges in Tandem Spot"
-      
+      @img1 = "bt.png"
+
       msj = @appointment.status 
       mail to: "#{user.name} <#{user.email}>", subject:"#{owner.name} has #{msj} a tandem with you"
     end
@@ -46,18 +49,21 @@ class AppointmentMailer < ActionMailer::Base
       @text1 = "Hello #{@owner.name},<br /> <br /> Congratulations you have a tandem request! :) \n #{@user.name} wants to speak with you."  
       @text2 = "#{@day} <br />#{@time}"
       @text3 = "Remember to login to Tandem Spot 10 minutes before your appointment and go to Speak Now"
+      @img1 = "bt3.png"
       msj = "requested"  
       mail to: "#{owner.name} <#{owner.email}>", subject:"#{user.name} has #{msj} a tandem with you"
     elsif @appointment.status =="accepted"
       @text1 = "Hello #{@owner.name},<br /> <br /> You have accepted a tandem with #{@user.name}! :) "  
       @text2 = "#{@day} <br />#{@time}"
       @text3 = "Remember to login to Tandem Spot 10 minutes before your appointment and go to Speak Now"
+      @img1 = "bt.png"
       msj = @appointment.status 
       mail to: "#{owner.name} <#{owner.email}>", subject:"You have #{msj} a tandem with #{user.name}"
     elsif @appointment.status =="canceled"
       @text1 = "Hello #{@owner.name},<br /> <br /> You have refused a tandem. We hope that you are busy doing funny things and you are not delaying your language learning forever... "  
       @text2 = "Canceled!"
       @text3 = "We are sure that you can find a great language partner that fits you."
+      @img1 = "bt2.png"
       msj = @appointment.status 
       mail to: "#{owner.name} <#{owner.email}>", subject:"You have #{msj} a tandem with #{user.name}"
     end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,6 @@ Tandem::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+  config.action_mailer.asset_host = 'http://localhost:3000'
+
 end
