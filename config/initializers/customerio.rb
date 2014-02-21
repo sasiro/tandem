22676e22cdccac84b5428c0f6f1982d51f89fde6
Victor Donet <victordp86@gmail.com>
2014-02-21 20:54:55 +0100

    costumer.io adapted for development

--- config/initializers/customerio.rb
+++ config/initializers/customerio.rb
@@ -1,7 +1,14 @@
 # config/initializers/customerio.rb
+if Rails.env.development?
+  Rails.configuration.customerio = {
+    :site_id      =>'f77a207194bb09719221' ,
+    :api_key      => '77e603f842bfff7e4abd'
+  }
+elsif Rails.env.production?
 Rails.configuration.customerio = {
   :site_id      => ENV['CIO_SITE_ID'],
   :api_key      => ENV['CIO_API_KEY']
 }
+end
   
 $customerio = Customerio::Client.new(Rails.configuration.customerio[:site_id], Rails.configuration.customerio[:api_key])
