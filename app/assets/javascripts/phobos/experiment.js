a2574f69796d85554bf4caee76c42ab41687b8a6
Victor Donet <victordp86@gmail.com>
2014-02-21 17:55:48 +0100

    GA code to check if people use more sign in with facebook from navbar or image

--- app/assets/javascripts/phobos/experiment.js
+++ app/assets/javascripts/phobos/experiment.js
@@ -3,7 +3,21 @@ $( document ).ready(function() {
 
 $( ".facebook-button" ).click(function() {
   
-   ga("send","event","registro-facebook","click");
+   
+   trak.io.track('Signed up');
+});
+});
+
+$( "#facebook-button-imagen" ).click(function() {
+  
+   ga("send","event","registro-facebook","click","imagen");
+   trak.io.track('Signed up');
+});
+});
+
+$( "#facebook-button-navbar" ).click(function() {
+  
+   ga("send","event","registro-facebook","click","navbar");
    trak.io.track('Signed up');
 });
 });
