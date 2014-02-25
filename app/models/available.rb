11be0798bff2ae2c551e249ac99aa20264643247
Victor Donet <victordp86@gmail.com>
2014-02-25 21:12:41 +0100

    seed for Available ready

--- app/models/available.rb
+++ app/models/available.rb
@@ -1,9 +1,9 @@
 class Available < ActiveRecord::Base
-  attr_accessible :day, :duration, :starts
-  
+  attr_accessible :day, :duration, :starts, :user_id
+
   has_many :appointments, dependent: :destroy
   belongs_to :user
-  
+
   DAYS = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday]
   def start_end_time
     '' +self.starts.strftime("%I:%M%p") + '-' + ((self.starts + self.duration)).strftime("%I:%M%p")+''
