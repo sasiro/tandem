9872bc7c93c9dac89bd1f3a357c370164d892174
Victor Donet <victordp86@gmail.com>
2014-02-27 15:09:15 +0100

    Experiment javascript loadin when dom ready

--- app/assets/javascripts/phobos/experiment.js
+++ app/assets/javascripts/phobos/experiment.js
@@ -2,22 +2,21 @@
 $( document ).ready(function() {
 
 $( ".facebook-button" ).click(function() {
-  
-   
+
+
    trak.io.track('Signed up');
-});
 });
 
 $( "#facebook-button-imagen" ).click(function() {
-  
+
    ga("send","event","registro-facebook","click","imagen");
    trak.io.track('Signed up');
 });
-});
 
 $( "#facebook-button-navbar" ).click(function() {
-  
+
    ga("send","event","registro-facebook","click","navbar");
    trak.io.track('Signed up');
 });
+
 });
